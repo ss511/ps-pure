2f8962b6b1ff29f37bc0247a12081bb301a1c800
ss511 <shashank.s4u@gmail.com>
2021-07-29 10:47:24 +0530

    [PS][DP] Coin change problem

---
+++ app/com/shashank/ps/dp/CoinChange.java
@@ -0,0 +1,53 @@
+package com.shashank.ps.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+/**
+ * Coin change problem.
+ * Given a set of coins and infinite supply of each coin and a give an input n.
+ * Find the number of ways the coins can be added up to the sum n.
+ *
+ * Uses bottom-up tabulation approach of DP.
+ */
+public class CoinChange {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int[] c = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        long ways = getWays(n, c);
+
+        System.out.println("Number of ways: " + ways);
+
+        bufferedReader.close();
+    }
+
+    private static long getWays(int n, int[] c) {
+        long[] table=new long[n+1];
+
+        // Base case (If given value is 0)
+        table[0] = 1;
+
+        // Pick all coins one by one and update the table[] values
+        // after the index greater than or equal to the value of the
+        // picked coin
+        for (int k : c) {
+            for (int j = k; j <= n; j++) {
+                table[j] += table[j - k];
+            }
+        }
+        return table[n];
+    }
+}
